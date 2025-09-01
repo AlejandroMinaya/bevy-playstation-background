72440e4e28677eb6e2cdbee308faef802ff7ba2b
Juan A <j.alejandrominaya@gmail.com>
2025-09-02 00:35:00 +0200

    tutorial: Implement our first plugin!

--- src/main.rs
+++ src/main.rs
@@ -22,20 +22,31 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Louise Belcher".to_string())));
 }
 
-fn hello_world() {
-    println!("Hello World!");
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
+        println!(" == DONE WITH GREETINGS == ");
+    }
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}!", name.0);
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(HelloPlugin)
         .run();
 }
