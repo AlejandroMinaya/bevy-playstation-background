a7fe86e68a89e6bc67e6aec74ca2f0de4579c45f
Juan A <j.alejandrominaya@gmail.com>
2025-09-03 00:17:49 +0200

    feat: Add staggered starts to the particles

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,32 @@ use bevy::prelude::*;
 
 const PARTICLE_SIZE: f32 = 10.0;
 const TOTAL_PARTICLES: i32 = 10;
+const STAGGER_DELAY_SCS: f32 = 0.25;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, move_particle)
+        .add_systems(Update, (advance_start_timers, move_particle).chain())
         .run();
 }
 
 #[derive(Component)]
 struct Particle;
 
-fn move_particle(time: Res<Time>, mut query: Query<&mut Transform, With<Particle>>) {
-    for mut transform in &mut query {
-        transform.translation.y += 20. * time.delta_secs()
+#[derive(Component)]
+struct StartTimer(Timer);
+
+fn move_particle(time: Res<Time>, mut query: Query<(&mut Transform, &StartTimer), With<Particle>>) {
+    for (mut transform, start_timer) in &mut query {
+        if start_timer.0.finished() {
+            transform.translation.y += 20. * time.delta_secs();
+        }
+    }
+}
+fn advance_start_timers(time: Res<Time>, mut query: Query<&mut StartTimer>) {
+    for mut timer in &mut query {
+        timer.0.tick(time.delta());
     }
 }
 
@@ -37,6 +48,10 @@ fn setup(
             Mesh2d(circle),
             MeshMaterial2d(materials.add(color)),
             Transform::from_xyz(x_pos, 0.0, 0.0),
+            StartTimer(Timer::from_seconds(
+                STAGGER_DELAY_SCS * id as f32,
+                TimerMode::Once,
+            )),
         ));
     }
 }
