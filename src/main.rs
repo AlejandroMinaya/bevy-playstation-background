933a747b69d4519d960d573113647d1edef9ce97
Juan A <j.alejandrominaya@gmail.com>
2025-09-02 23:44:18 +0200

    feat: Add particle moving really slowly

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,17 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, move_particle)
         .run();
+}
+
+#[derive(Component)]
+struct Particle;
+
+fn move_particle(time: Res<Time>, mut query: Query<&mut Transform, With<Particle>>) {
+    for mut transform in &mut query {
+        transform.translation.x += 20. * time.delta_secs()
+    }
 }
 
 fn setup(
@@ -17,6 +27,7 @@ fn setup(
     let color = Color::oklaba(1.0, 0.0, 0.0, 1.0);
 
     commands.spawn((
+        Particle,
         Mesh2d(circle),
         MeshMaterial2d(materials.add(color)),
         Transform::from_xyz(0.0, 0.0, 0.0),
