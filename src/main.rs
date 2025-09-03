9a2858ce93fb09f5fd4717eed67ebed0c6b62b20
Juan A <j.alejandrominaya@gmail.com>
2025-09-03 21:20:24 +0200

    feat: Add sine wave

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ const MAX_FPS: f64 = 120.0;
 const MIN_PERIOD: f32 = 1.0 / MAX_FPS as f32;
 
 const PARTICLE_SIZE: f32 = 2.0;
-const PARTICLE_SPEED: f32 = 1.0;
 const TOTAL_PARTICLES: i32 = 300;
-const FREQUENCY: f32 = 100.0;
+const FREQUENCY: f32 = 5.0;
 const PERIOD: f32 = 1.0 / FREQUENCY;
-const AMPLITUDE: f32 = 100.0;
+const AMPLITUDE: f32 = 10.0;
 
 fn main() {
     App::new()
@@ -25,24 +24,39 @@ fn main() {
 #[derive(Component)]
 struct StartTimer(Timer);
 
-fn advance_all_start_timers(time: Res<Time>, mut query: Query<&mut StartTimer>) {
-    for mut timer in &mut query {
+fn advance_all_start_timers(time: Res<Time>, mut query: Query<(&mut StartTimer, &mut Particle)>) {
+    for (mut timer, mut particle) in &mut query {
         timer.0.tick(time.delta());
+        if timer.0.just_finished() {
+            particle.start_time = time.elapsed_secs();
+        }
     }
 }
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
+#[derive(Component, Default)]
+struct Particle {
+    start_time: f32,
 }
+/*
+* y = x or sin(x)
+* x = time_elapsed
+* dy = transform.translation.y
+*/
 
-fn move_particle(mut query: Query<(&mut Transform, &mut Direction, &StartTimer)>) {
-    for (mut transform, mut direction, start_timer) in &mut query {
+fn y(x: f32) -> f32 {
+    x.sin()
+}
+fn move_particle(time: Res<Time>, mut query: Query<(&mut Transform, &StartTimer, &Particle)>) {
+    for (mut transform, start_timer, particle) in &mut query {
         if start_timer.0.finished() {
+            let x = time.elapsed_secs() - particle.start_time;
+            println!("X: {}", x);
+            transform.translation.y = y(x * FREQUENCY) * AMPLITUDE;
+            println!("Translation Y: {}", transform.translation.y);
+            /*
             match *direction {
-                Direction::Up => transform.translation.y += PARTICLE_SPEED,
-                Direction::Down => transform.translation.y -= PARTICLE_SPEED,
+                Direction::Up => transform.translation.y = x.sin(),
+                Direction::Down => transform.translation.y = (-x).sin(),
             }
 
             if transform.translation.y >= AMPLITUDE {
@@ -50,6 +64,7 @@ fn move_particle(mut query: Query<(&mut Transform, &mut Direction, &StartTimer)>
             } else if transform.translation.y <= -AMPLITUDE {
                 *direction = Direction::Up;
             }
+            */
         }
     }
 }
@@ -69,12 +84,12 @@ fn setup(
         commands.spawn((
             Mesh2d(circle),
             MeshMaterial2d(materials.add(color)),
-            Direction::Up,
             Transform::from_xyz(x_pos, 0.0, 0.0),
             StartTimer(Timer::from_seconds(
                 PERIOD.clamp(MIN_PERIOD, 1.0) * id as f32,
                 TimerMode::Once,
             )),
+            Particle::default(),
         ));
     }
 }
